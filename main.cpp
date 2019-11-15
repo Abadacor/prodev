a1eb52bcc937e4cc78fd6bf1c07387a2065b878f
Abadacor <romainhe.brisse@gmail.com>
2019-11-15 11:31:52 +0100

    linked SQL + test in main

--- main.cpp
+++ main.cpp
@@ -1,7 +1,25 @@
 #include <QApplication>
+#include <QtSql>
 #include <QDebug>
+#include<QWidget>
+#include<QSpinBox>
+#include<QSlider>
+#include<QHBoxLayout>
+#include <iostream>
 
-int main() {
+int main(int argc, char * argv[]) {
     qDebug() << QT_VERSION_STR;
+
+    //Test SQL
+    QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
+    db.setDatabaseName("test.db");
+
+    // Nom du fichier SQLite
+    if(!db.open()){
+        std::cout <<"Unable to open the database."<< endl;
+    }
+
+    db.close ();
+
     return 0;
 }
